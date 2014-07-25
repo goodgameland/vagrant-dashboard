7a25d1631967f463667e3bc771049135d3f4850a
David Thomas <dev@synth3.tk>
2014-07-25 00:05:56 -0400

    Dashing install in bootstrap

--- Vagrantfile
+++ Vagrantfile
@@ -1,3 +1,4 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "puppetlabs/debian-7.4-64-puppet"
+  config.vm.provision :shell, path: "bootstrap.sh"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+echo "deb http://ftp.us.debian.org/debian wheezy-backports main" >> /etc/apt/sources.list
 apt-get update
-apt-get install -y ruby1.9.1-dev
+apt-get install -y ruby1.9.1-dev nodejs
+gem install bundle
 gem install dashing
+dashing new lan_dashboard
+cd lan_dashboard
+bundle
+dashing start
